b35c7aacb59791b8a862dabea73073df5b42ad91
sibyg <meetsiby@gmail.com>
2017-07-30 21:44:13 +0100

    week1 labs : improve common package

--- src/main/scala/common/package.scala
+++ src/main/scala/common/package.scala
@@ -8,7 +8,6 @@ package object common {
 
   abstract class TaskScheduler {
     def schedule[T](body: => T): ForkJoinTask[T]
-
     def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
       val right = task {
         taskB
@@ -23,7 +22,12 @@ package object common {
       val t = new RecursiveTask[T] {
         def compute = body
       }
-      forkJoinPool.execute(t)
+      Thread.currentThread match {
+        case wt: ForkJoinWorkerThread =>
+          t.fork()
+        case _ =>
+          forkJoinPool.execute(t)
+      }
       t
     }
   }
@@ -40,17 +44,10 @@ package object common {
   }
 
   def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
-    val ta = task {
-      taskA
-    }
-    val tb = task {
-      taskB
-    }
-    val tc = task {
-      taskC
-    }
+    val ta = task { taskA }
+    val tb = task { taskB }
+    val tc = task { taskC }
     val td = taskD
     (ta.join(), tb.join(), tc.join(), td)
   }
-
 }
